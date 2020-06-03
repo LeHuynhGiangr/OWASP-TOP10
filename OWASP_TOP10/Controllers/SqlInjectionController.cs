5882fd31a030608c49023d600931a410f6d7d1cb
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2020-06-03 11:03:55 +0700

    Add project files.

---
+++ OWASP_TOP10/Controllers/SqlInjectionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OWASP_TOP10.Controllers
+{
+    public class SqlInjectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# ASP.NET Core 3.1
+## Technologies
+- ASP.NET Core 3.1
+-
+## Development environment and tools
+- .net core SDK 3.1(sorfware development kit)
+- IDE visual studio 2019 – professional
+- SQL server 2019
+## Install Packages
+-
+## How to configure and run
+## How to contribute
+## Contact
+- lehuynhgiang1219@outlook.com
