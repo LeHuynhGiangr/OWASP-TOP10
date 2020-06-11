911d3a00036b5cc3cdb0419f914e135a11982e69
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2020-06-12 00:44:17 +0700

    create library for working with sql server

---
+++ Data/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    class Utility
+    {
+        private const string connectionString = @"Data Source=<hostName>\<instanceName>; Initial Catalog=; User ID=; Password=";
+        private SqlConnection sqlConnection = new SqlConnection { ConnectionString = connectionString };
+
+        //using command with parameters
+        public static object Query(SqlCommand command)
+        {
+            try
+            {
+                
+            }
+            catch(SqlException sqlException)
+            {
+
+            }
+            return 0;
+        }
+
+        //using command without parameter
+        public static object Query(String command)
+        {
+            return 0;
+        }
+    }
+}
---
+++ OWASP_TOP10/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OWASP_TOP10.Models
+{
+    public class Account
+    {
+    }
+}
