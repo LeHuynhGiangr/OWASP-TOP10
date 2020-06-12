2b7e2ade83fe6b7a66784c4f2ff0ebc51833e765
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2020-06-12 20:59:43 +0700

    add product-view model

---
+++ OWASP_TOP10/Models/ViewProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OWASP_TOP10.Models
+{
+    public class ViewProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public float Price { get; set; }
+    }
+}
